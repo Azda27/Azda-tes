78e45c9ad8c22b55185b046bdb820c691d40b1de
Azda27 <growtopiagame875236@gmail.com>
2023-02-26 15:11:12 +0800

    Add and Remove

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// Hamburger Menu
 const menuToggle = document.querySelector('.menu-toggle input');
 const nav = document.querySelector('nav ul');
 
 menuToggle.addEventListener( 'click', function () {
   nav.classList.toggle( 'slide');
 });
+
