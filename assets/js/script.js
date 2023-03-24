6a3aead2d869d6de0f70b6863f1779455f56ad26
Achmad zailani <growtopiagame875236@gmail.com>
2023-03-24 20:46:14 +0800

    Update v1.7

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,46 @@
-const m = document.querySelector(".menu-toggle input"),
-  n = document.querySelector("nav ul");
-m.addEventListener("click", () => {
-  n.classList.toggle("slide");
+const menuToggle = document.querySelector(".menu-toggle input");
+const nav = document.querySelector("nav ul");
+menuToggle.addEventListener("click", () => {
+  nav.classList.toggle("slide");
 });
-const d = document.getElementById("dark-mode-switch"),
-  b = document.querySelector("body");
-let t = localStorage.getItem("themePreference");
-if (!t) {
-  (t = "auto"), localStorage.setItem("themePreference", t);
+const darkModeSwitch = document.getElementById("dark-mode-switch");
+const body = document.querySelector("body");
+let themePreference = localStorage.getItem("themePreference");
+if (!themePreference) {
+  themePreference = "auto";
+  localStorage.setItem("themePreference", themePreference);
 }
-"dark" === t
-  ? (enableDarkMode(), (d.checked = !0))
-  : "light" === t
-  ? (disableDarkMode(), (d.checked = !1))
-  : (window.matchMedia &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches &&
-      (enableDarkMode(), (d.checked = !0)),
-    d.addEventListener("change", function () {
-      this.checked
-        ? (enableDarkMode(), localStorage.setItem("themePreference", "dark"))
-        : (disableDarkMode(), localStorage.setItem("themePreference", "light"));
-    })),
-  (clearLocalStorageBtn = document.getElementById("clear")),
-  clearLocalStorageBtn.addEventListener("click", () => {
-    localStorage.clear();
-  });
+if (themePreference === "dark") {
+  enableDarkMode();
+  darkModeSwitch.checked = !0;
+} else if (themePreference === "light") {
+  disableDarkMode();
+  darkModeSwitch.checked = !1;
+} else {
+  if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  ) {
+    enableDarkMode();
+    darkModeSwitch.checked = !0;
+  }
+}
+darkModeSwitch.addEventListener("change", function () {
+  if (this.checked) {
+    enableDarkMode();
+    localStorage.setItem("themePreference", "dark");
+  } else {
+    disableDarkMode();
+    localStorage.setItem("themePreference", "light");
+  }
+});
 function enableDarkMode() {
-  b.classList.add("dark-mode");
+  body.classList.add("dark-mode");
 }
 function disableDarkMode() {
-  b.classList.remove("dark-mode");
+  body.classList.remove("dark-mode");
 }
+const clearLocalStorageBtn = document.getElementById("clear");
+clearLocalStorageBtn.addEventListener("click", () => {
+  localStorage.clear();
+});
